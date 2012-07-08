678d1793f68085dee9d92bccd313c14b2c6059ed
edwher.123@hotmail.com <edwher.123@hotmail.com@ba12e74f-bd48-215f-c038-9413ea825e3c>
2012-07-08 19:41:58 +0000

    Cambios sobre mensajeria. Lista Mensajes

---
+++ MaizSoft/src/main/com/mydomain/Directorio/model/ConsultasJpql.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.mydomain.Directorio.model;
+
+/**
+ * Descripcion: Esta Clase se encarga de ... Modulo de Desarrollo :CU- ...
+ * 
+ * @author Edwin Jose Hernandez Niño edwher.123@hotmail.com
+ * @author Josue Nicolas Pinzon Villamil jnpinzonv@hotmail.com Grupo de
+ *         Investigacion Ingenieria Software (GIS) Semillero de Investigacion
+ *         Moviles Sabatt (SIMS) Universidad Pedagogica y Tecnologica de
+ *         Colombia
+ * @version 08/07/2012
+ */
+public interface ConsultasJpql {
+
+	String LISTA_MENSAJES_USUARIO = "SELECT g FROM ReceptorMensajes r, GestorMensajeria g, CuentasUsuario u, UserAccount ua"
+			+ " WHERE u.userAccount.id = ua.id"
+			+ " AND u.usuarios.id = r.usuarios.id"
+			+ " AND r.gestorMensajeria.idMensaje = g.idMensaje"
+			+ " AND ua.username =:parametro";
+	
+	String USUARIO_POR_USERNAME = "select u from UserAccount ua, Usuario u, CuentasUsuario cu where ua.username =:parametro" +
+		    " and ua.id = cu.userAccounts.id" +
+		    " and cu.usuarios.id = u.id";
+
+}
